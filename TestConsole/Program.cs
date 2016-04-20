83eb708b2ffcbba14d3de36e14533c6ece6e019c
madks13 <nikola.tomasevic@epitech.eu>
2016-04-20 18:28:27 +0200

    Add : NumberToBase converter Add : TestingConsole project for testing in console

---
+++ TestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.Adapters;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 9081726354;
+            NumberToBase b2b = new NumberToBase("findme");
+            String s = b2b.Base10ToString(n);
+            Console.WriteLine(s);
+            Console.WriteLine(b2b.StringToBase10(s));
+            Console.ReadKey();
+        }
+    }
+}
